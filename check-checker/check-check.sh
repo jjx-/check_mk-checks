06c9cd23b3c860907d0b3546c17c520faa3491b9
AKA-47 <AKA-47@users.noreply.github.com>
2016-12-06 12:02:48 +0100

    Script zur Überprüfung ob Agent output bestimmte Sektion enthält
    # Script zur Überprüfung ob Agent output bestimmte Sektion enthält
    # AKA: ein check der checkt ob ein check gecheckt wird (TM)

---
+++ check-checker/check-check.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Script zur Überprüfung ob Agent output bestimmte Sektion enthält
+# AKA: ein check der checkt ob ein check gecheckt wird (TM)
+# $1 ist Hostname
+# $2 ist sektion z.b. apt
+
+# Aufruf: 
+
+HOSTNAME=$1
+SECTION=$2
+#  grep "^<<<apt>>>" Line Starts with - agent output
+# nc -w secs         timeout for connects and final net reads
+
+
+SETISOK=$(nc $HOSTNAME 6556 -w 5 |  grep "^<<<$SECTION>>>")
+
+if [ -z "$SETISOK" ]; then
+    echo "CRITICAL - MONITORINGSET $SECTION CHECK IS NOT THERE"
+exit 1
+else
+    echo "OK -  MONITORINGSET $SECTION CHECK IS THERE"
+fi
